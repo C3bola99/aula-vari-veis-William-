60bd8cc1827311a00e824c3b3c7ac3412c852bbc
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-17 23:17:04 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,14 @@
+// Fazendo as perguntas
+let pergunta1 = prompt("Você está usando uma roupa azul hoje? (sim ou não)");
+let pergunta2 = prompt("Você gosta de comer pizza? (sim ou não)");
+let pergunta3 = prompt("Você já visitou outro país? (sim ou não)");
+
+// Criando novas variáveis contendo as respostas
+let resposta1 = "Sim";
+let resposta2 = "Não";
+let resposta3 = "Não";
+
+// Imprimindo na tela todas as perguntas seguidas por suas respectivas respostas
+console.log("Você está usando uma roupa azul hoje? " + resposta1);
+console.log("Você gosta de comer pizza? " + resposta2);
+console.log("Você já visitou outro país? " + resposta3);

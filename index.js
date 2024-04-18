e64b523380480f318c25575950dcacf180699f36
William Wieser <166855277+C3bola99@users.noreply.github.com>
2024-04-17 23:19:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let nome;
+let idade;
+
+// Perguntar ao usuário seu nome
+nome = prompt("Qual é o seu nome?");
+
+// Perguntar ao usuário sua idade e converter para número
+idade = prompt("Qual é a sua idade?");
+
+// Imprimir a mensagem na tela
+console.log("Olá " + nome + ", você tem " + idade + " anos.");
+
